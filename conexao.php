02b748211fb5e8e40ad24bde45e59c6c05189f51
dm201085 <debora.dantas10@aluno.ce.gov.br>
2026-06-09 21:51:13 -0300

    ajeitando o banco de dados pela 2°vez

--- conexao.php
+++ conexao.php
@@ -1,14 +1,21 @@
 <?php
 
-$host = "db";
+$host = "localhost";
 $usuario = "root";
 $senha = "root";
 $database = "ingresso";
 
-$mysqli = new mysqli($host, $usuario, $senha, $database);
+// Instancia a classe mysqli para criar a conexão (nota: aqui está passando uma senha vazia "" ao invés da variável $senha)
+$mysqli = new mysqli($host, $usuario, "", $database);
 
+// Verifica se o objeto retornou algum código de erro na tentativa de conexão
 if ($mysqli->connect_errno) {
     die("Erro ao conectar ao banco: " . $mysqli->connect_error);
 }
 
+if (!$mysqli) {
+    die("Erro na conexão: " . mysqli_connect_error());
+}
+
+// Configura o padrão de caracteres da conexão para evitar problemas com acentuação e emojis no banco de dados
 $mysqli->set_charset("utf8mb4");
